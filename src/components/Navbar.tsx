f4d8730883fecfc88c6d31be21fdbb9f5d03bf90
Louis <louis.wackenier@gmail.com>
2024-11-12 08:34:02 +0100

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import styled from "styled-components";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <NavbarContainer>
+        <HomeButton>Home</HomeButton>  
+
+        <RightButtonContainer>
+           <PortfolioButton>Portfolio</PortfolioButton>
+           <ContactButton>Contact me</ContactButton>
+        </RightButtonContainer>
+    </NavbarContainer>
   )
 }
 
 export default Navbar
+
+const NavbarContainer = styled.div`
+    height: 10vh;
+`;
+const HomeButton = styled.button`
+      padding: 1vh 2vw;
+      margin: 1vh;
+      display: inline-block;
+`;
+
+const RightButtonContainer = styled.div`
+      display: inline-block;
+      position: absolute;
+      right: 3%;
+`;
+
+const PortfolioButton = styled.button`
+`;
+
+const ContactButton = styled.button`
+`;
