ae03869e460a918a21ba2f91705ba4780390467d
Louis <louis.wackenier@gmail.com>
2024-11-13 07:44:47 +0100

    buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,22 +16,51 @@ const Navbar = () => {
 export default Navbar
 
 const NavbarContainer = styled.div`
-    height: 10vh;
+      height: 10vh;
 `;
 const HomeButton = styled.button`
       padding: 1vh 2vw;
       margin: 1vh;
       display: inline-block;
+      border-radius: 5px;
+      border: 3px solid lightblue;
+      transition: all 0.5s;
+
+      &:hover {
+        background-color: blue;
+      }
 `;
 
 const RightButtonContainer = styled.div`
       display: inline-block;
       position: absolute;
       right: 3%;
+
+      
 `;
 
 const PortfolioButton = styled.button`
+      margin: 1vh;
+      padding: 1vh 2vw;
+      border-radius: 5px;
+      border: 3px solid lightblue;
+
+      transition: all 0.5s;
+
+      &:hover {
+        background-color: blue;
+      }
 `;
 
 const ContactButton = styled.button`
+      padding: 1vh 2vw;
+      margin-left: 1vw;
+      border-radius: 5px;
+      border: 3px solid lightblue;
+
+      transition: all 0.5s;
+
+      &:hover {
+        background-color: blue;
+      }
 `;
