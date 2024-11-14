a7be2dfbf923e0cfe849ef8e222cff24608799cd
Louis <louis.wackenier@gmail.com>
2024-11-14 08:56:49 +0100

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,10 @@ import styled from "styled-components"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeContainer>
+        <MainTitle>Welcome to my new Portfolio site</MainTitle>
+        <SubTitle>Take a look around :D</SubTitle>
+    </HomeContainer>
   )
 }
 
@@ -10,4 +13,13 @@ export default Home
 
 const HomeContainer = styled.div`
         
+`;
+
+const MainTitle = styled.h1`
+      text-align: center;
+      margin-top:10vh;
+`;
+
+const SubTitle = styled.h2`
+      text-align: center;
 `;
