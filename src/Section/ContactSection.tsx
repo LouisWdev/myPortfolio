426f1b1889ebe2b8ec5f97a132c67e0b3d2c9a64
Louis <louis.wackenier@gmail.com>
2024-12-17 14:33:17 +0100

    contact form container

--- src/Section/ContactSection.tsx
+++ src/Section/ContactSection.tsx
@@ -17,9 +17,15 @@ export default ContactSection
 /* STYLED COMPONENTS */
 const MainContainer = styled.div`
      height: 90vh;
+     display: flex;
+     justify-content: center;
+     align-items: center;
 `;
 
 const FormContainer = styled.div`
+     padding: 3vh 3vw;
+     border: 1vh solid white;
+     border-radius: 1vh;
 `;
 
 const EmailContainer = styled.form`
