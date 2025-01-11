48b44a151c8cfd7028da1665275c44dd1e20235d
Louis <louis.wackenier@gmail.com>
2025-01-11 10:08:08 +0100

    Portfolio changes

--- src/Section/PortfolioSection.tsx
+++ src/Section/PortfolioSection.tsx
@@ -1,4 +1,12 @@
-import styled from "styled-components"
+// styled comps import
+import styled from "styled-components";
+// three js
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
 
 const PortfolioSection = () => {
   return (
@@ -8,6 +16,8 @@ const PortfolioSection = () => {
          <ProjectCardImage>Image goes here</ProjectCardImage>
          <ProjectCardText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas.</ProjectCardText>
          <ProjectCardButton>Go to project</ProjectCardButton>
+
+   
        </ProjectCard>
 
        <SkillSection>
