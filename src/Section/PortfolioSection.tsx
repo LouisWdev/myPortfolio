bceee0a7e3049962e183832b1ee6c78e9fbfec1c
Louis <louis.wackenier@gmail.com>
2025-01-25 08:03:22 +0100

    Implemented PortfolioProject component

--- src/Section/PortfolioSection.tsx
+++ src/Section/PortfolioSection.tsx
@@ -1,17 +1,13 @@
 // styled comps import
 import styled from "styled-components";
 import Footer from "../components/Footer";
+import PortfolioProject from "../components/PortfolioProject";
 
 const PortfolioSection = () => {
   return (
     <MainContainer>
        <ProjectsSection>
-          <ProjectCard>
-            <ProjectCardTitle>First Project</ProjectCardTitle>
-            <ProjectCardImage>Image goes here</ProjectCardImage>
-            <ProjectCardText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas.</ProjectCardText>
-            <ProjectCardButton>Go to project</ProjectCardButton>
-          </ProjectCard>
+          <PortfolioProject />
        </ProjectsSection>
 
        <SkillSection>
@@ -63,44 +59,6 @@ const ProjectsSection = styled.div`
      padding-bottom: 40vh;
      background-color: #061f2b;
 `;
-
-const ProjectCard = styled.div`
-      border: solid white 1vh;
-      width: 20vw;
-      height: 50vh;
-      border-radius: 20px;
-`;
-
-const ProjectCardTitle = styled.h1`
-    padding: 1vh 1vw;
-`;
-
-const ProjectCardText = styled.p`
-    margin-top: 14vh;
-    padding: 2vh 2vw;
-`;
-
-const ProjectCardImage = styled.image`
-    height: 5vh;
-    margin: 5vh 5vw;
-`;
-
-const ProjectCardButton = styled.button`
-      cursor: pointer;
-      padding: 2vh 4vw;
-      color: black;
-      background-color: yellow;
-      margin: 1vh 3vw;
-      border: 0.5vh solid white;
-      border-radius: 1vh;
-      transition: 500ms all;
-
-      &:hover {
-          color: white;
-          background-color: #494900;
-      }
-`;
-
 
 /* Skills Section */
 const SkillSection = styled.div`
---
+++ src/components/PortfolioProject.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+const PortfolioProject = () => {
+  return (
+    <>
+     <ProjectCard>
+            <ProjectCardTitle> Project one </ProjectCardTitle>
+            <ProjectCardImage>Image goes here</ProjectCardImage>
+            <ProjectCardText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas.</ProjectCardText>
+            <ProjectCardButton>Go to project</ProjectCardButton>
+          </ProjectCard>
+     <ProjectCard />
+    </>
+  )
+}
+
+export default PortfolioProject
+
+/* PROJECT CARD */
+const ProjectCard = styled.div`
+      border: solid white 1vh;
+      width: 20vw;
+      height: 50vh;
+      border-radius: 20px;
+`;
+
+const ProjectCardTitle = styled.h1`
+    padding: 1vh 1vw;
+`;
+
+const ProjectCardText = styled.p`
+    margin-top: 14vh;
+    padding: 2vh 2vw;
+`;
+
+const ProjectCardImage = styled.image`
+    height: 5vh;
+    margin: 5vh 5vw;
+`;
+
+const ProjectCardButton = styled.button`
+      cursor: pointer;
+      padding: 2vh 4vw;
+      color: black;
+      background-color: yellow;
+      margin: 1vh 3vw;
+      border: 0.5vh solid white;
+      border-radius: 1vh;
+      transition: 500ms all;
+
+      &:hover {
+          color: white;
+          background-color: #494900;
+      }
+`;
